11e4fc296e611b64995882a779a29addae914de9
KRISH KUNAL <kunalkrish3350@gmail.com>
2026-08-03 10:38:18 +0530

    Time: 67 ms (50.04%), Space: 111.3 MB (39.4%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> st;
+        for(int num : nums)
+        {
+            if(st.find(num) != st.end())
+            {
+                return true;
+            }
+            st.insert(num);
+        }
+        return false;
+        
+    }
+};
